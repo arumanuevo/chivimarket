3ed5c4f29630513ef4a70e7b0f0fc5ec73c31a2e
arumanuevo <glipischeck@gmail.com>
2025-09-15 22:32:05 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,13 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index()
-    {
-        
+{
+    try {
         return response()->json(User::all());
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
     }
+}
+
 }
 
