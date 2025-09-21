9ecdb1a5f047542409e410a1ce22c91532e247a7
arumanuevo <glipischeck@gmail.com>
2025-09-21 12:38:14 -0300

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Providers;
+/*namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
@@ -11,5 +11,42 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
         //Passport::hashClientSecrets();
        // Passport::tokensExpireIn(now()->addDays(15));
+    }
+}*/
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+         *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        // Registrar rutas y configuración de Passport
+       // if (! $this->app->routesAreCached()) {
+           // Passport::routes();
+        //}
+
+        // Configurar el tiempo de expiración de los tokens
+        //Passport::tokensExpireIn(now()->addDays(15));
+        //Passport::refreshTokensExpireIn(now()->addDays(30));
     }
 }
